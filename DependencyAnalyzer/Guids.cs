d248e13c4e425e9450cbf689f06b136848e09758
foxy1982 <daniel.fox@ukgateway.net>
2013-10-27 19:59:23 +0000

    Initial checkin

---
+++ DependencyAnalyzer/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace KitsuneSoft.DependencyAnalyzer
+{
+    static class GuidList
+    {
+        public const string guidDependencyAnalyzerPkgString = "14e18505-a7ef-4bc2-8bed-d49c0b5062a7";
+        public const string guidDependencyAnalyzerCmdSetString = "3af5c766-798c-429e-8414-17ab25fd996e";
+        public const string guidToolWindowPersistanceString = "ed24c3ba-1e06-492c-9c83-40632d21eaf7";
+
+        public static readonly Guid guidDependencyAnalyzerCmdSet = new Guid(guidDependencyAnalyzerCmdSetString);
+    };
+}
---
+++ DependencyAnalyzer/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace KitsuneSoft.DependencyAnalyzer
+{
+    static class PkgCmdIDList
+    {
+
+        public const uint cmdidViewDependencies =    0x101;
+
+    };
+}
